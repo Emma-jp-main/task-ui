90f7408ab478c623a8e6ffb0075b107908e62f64
Emma-jp-main <mathlogue3431@gmail.com>
2026-06-27 16:57:28 +0900

    feat: task一覧表示

--- src/TaskApp.jsx
+++ src/TaskApp.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from "react";
 import './App.css'
 
 function TaskApp() {
-  return (
-      <div>
-        <h1>Task App</h1>
-      </div>
-  )
+    const [tasks, setTasks] = useState([]);
+
+    //fetchしてとってきたデータをsetTasksに入れたい
+    useEffect(() => {
+        const fetchTasks = async () => {
+            const response = await fetch('/api/tasks');
+            console.log("status=", response.status);
+            const data = await response.json();
+            console.log("data=", data);
+            setTasks(data);
+        }
+        fetchTasks();
+    }, []);
+
+    return (
+       <div>
+           <h1>Task App</h1>
+           <ul>
+               {tasks.map((task) => (
+                   <li key={task.id}>
+                       {/*{task.completed}だと表示されないので注意*/}
+                       {/*{boolean}は表示されない*/}
+                       {task.title}:{task.completed ? "完了" : "未完了"}
+                   </li>
+               ))}
+           </ul>
+       </div>
+    );
 }
 
 export default TaskApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import TaskApp from './TaskApp.jsx'
+import TaskApp from "./TaskApp.jsx";
 
+//index.htmlのrootに、TaskAppコンポーネントをReactとして表示してね
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <TaskApp />
-  </StrictMode>,
-)
+    //StrictModeでチェック強化
+    <StrictMode>
+        <TaskApp/>
+    </StrictMode>,
+);
